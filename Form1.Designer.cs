ff01c693efd90ce924c0575ee7244c0912af3917
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-04-11 22:18:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace picturebox_e_radiobutton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBoxAssalto1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxAssalto2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxAssalto3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEscopeta1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEscopeta2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEscopeta3 = new System.Windows.Forms.PictureBox();
+            this.btnFuzilsDeAssalto = new System.Windows.Forms.RadioButton();
+            this.btnAssalto1 = new System.Windows.Forms.RadioButton();
+            this.btnEscopetas = new System.Windows.Forms.RadioButton();
+            this.btnEscopeta1 = new System.Windows.Forms.RadioButton();
+            this.btnAssalto2 = new System.Windows.Forms.RadioButton();
+            this.btnEscopeta2 = new System.Windows.Forms.RadioButton();
+            this.btnAssalto3 = new System.Windows.Forms.RadioButton();
+            this.btnEscopeta3 = new System.Windows.Forms.RadioButton();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxAssalto1
+            // 
+            this.pictureBoxAssalto1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxAssalto1.Image")));
+            this.pictureBoxAssalto1.Location = new System.Drawing.Point(55, 121);
+            this.pictureBoxAssalto1.Name = "pictureBoxAssalto1";
+            this.pictureBoxAssalto1.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxAssalto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxAssalto1.TabIndex = 0;
+            this.pictureBoxAssalto1.TabStop = false;
+            this.pictureBoxAssalto1.Visible = false;
+            // 
+            // pictureBoxAssalto2
+            // 
+            this.pictureBoxAssalto2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxAssalto2.Image")));
+            this.pictureBoxAssalto2.Location = new System.Drawing.Point(55, 376);
+            this.pictureBoxAssalto2.Name = "pictureBoxAssalto2";
+            this.pictureBoxAssalto2.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxAssalto2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxAssalto2.TabIndex = 1;
+            this.pictureBoxAssalto2.TabStop = false;
+            this.pictureBoxAssalto2.Visible = false;
+            // 
+            // pictureBoxAssalto3
+            // 
+            this.pictureBoxAssalto3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxAssalto3.Image")));
+            this.pictureBoxAssalto3.Location = new System.Drawing.Point(55, 625);
+            this.pictureBoxAssalto3.Name = "pictureBoxAssalto3";
+            this.pictureBoxAssalto3.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxAssalto3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxAssalto3.TabIndex = 2;
+            this.pictureBoxAssalto3.TabStop = false;
+            this.pictureBoxAssalto3.Visible = false;
+            // 
+            // pictureBoxEscopeta1
+            // 
+            this.pictureBoxEscopeta1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxEscopeta1.Image")));
+            this.pictureBoxEscopeta1.Location = new System.Drawing.Point(606, 121);
+            this.pictureBoxEscopeta1.Name = "pictureBoxEscopeta1";
+            this.pictureBoxEscopeta1.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxEscopeta1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxEscopeta1.TabIndex = 3;
+            this.pictureBoxEscopeta1.TabStop = false;
+            this.pictureBoxEscopeta1.Visible = false;
+            // 
+            // pictureBoxEscopeta2
+            // 
+            this.pictureBoxEscopeta2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxEscopeta2.Image")));
+            this.pictureBoxEscopeta2.Location = new System.Drawing.Point(606, 376);
+            this.pictureBoxEscopeta2.Name = "pictureBoxEscopeta2";
+            this.pictureBoxEscopeta2.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxEscopeta2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxEscopeta2.TabIndex = 4;
+            this.pictureBoxEscopeta2.TabStop = false;
+            this.pictureBoxEscopeta2.Visible = false;
+            // 
+            // pictureBoxEscopeta3
+            // 
+            this.pictureBoxEscopeta3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxEscopeta3.Image")));
+            this.pictureBoxEscopeta3.Location = new System.Drawing.Point(606, 625);
+            this.pictureBoxEscopeta3.Name = "pictureBoxEscopeta3";
+            this.pictureBoxEscopeta3.Size = new System.Drawing.Size(294, 208);
+            this.pictureBoxEscopeta3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxEscopeta3.TabIndex = 5;
+            this.pictureBoxEscopeta3.TabStop = false;
+            this.pictureBoxEscopeta3.Visible = false;
+            // 
+            // btnFuzilsDeAssalto
+            // 
+            this.btnFuzilsDeAssalto.AutoSize = true;
+            this.btnFuzilsDeAssalto.Location = new System.Drawing.Point(151, 72);
+            this.btnFuzilsDeAssalto.Name = "btnFuzilsDeAssalto";
+            this.btnFuzilsDeAssalto.Size = new System.Drawing.Size(157, 24);
+            this.btnFuzilsDeAssalto.TabIndex = 6;
+            this.btnFuzilsDeAssalto.TabStop = true;
+            this.btnFuzilsDeAssalto.Text = "Fuzils De Assalto";
+            this.btnFuzilsDeAssalto.UseVisualStyleBackColor = true;
+            this.btnFuzilsDeAssalto.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnAssalto1
+            // 
+            this.btnAssalto1.AutoSize = true;
+            this.btnAssalto1.Location = new System.Drawing.Point(401, 215);
+            this.btnAssalto1.Name = "btnAssalto1";
+            this.btnAssalto1.Size = new System.Drawing.Size(159, 24);
+            this.btnAssalto1.TabIndex = 7;
+            this.btnAssalto1.TabStop = true;
+            this.btnAssalto1.Text = "Fuzil de Assalto 1";
+            this.btnAssalto1.UseVisualStyleBackColor = true;
+            this.btnAssalto1.Visible = false;
+            // 
+            // btnEscopetas
+            // 
+            this.btnEscopetas.AutoSize = true;
+            this.btnEscopetas.Location = new System.Drawing.Point(678, 71);
+            this.btnEscopetas.Name = "btnEscopetas";
+            this.btnEscopetas.Size = new System.Drawing.Size(110, 24);
+            this.btnEscopetas.TabIndex = 8;
+            this.btnEscopetas.TabStop = true;
+            this.btnEscopetas.Text = "Escopetas";
+            this.btnEscopetas.UseVisualStyleBackColor = true;
+            this.btnEscopetas.CheckedChanged += new System.EventHandler(this.btnEscopetas_CheckedChanged);
+            // 
+            // btnEscopeta1
+            // 
+            this.btnEscopeta1.AutoSize = true;
+            this.btnEscopeta1.Location = new System.Drawing.Point(951, 214);
+            this.btnEscopeta1.Name = "btnEscopeta1";
+            this.btnEscopeta1.Size = new System.Drawing.Size(115, 24);
+            this.btnEscopeta1.TabIndex = 9;
+            this.btnEscopeta1.TabStop = true;
+            this.btnEscopeta1.Text = "Escopeta 1";
+            this.btnEscopeta1.UseVisualStyleBackColor = true;
+            this.btnEscopeta1.Visible = false;
+            // 
+            // btnAssalto2
+            // 
+            this.btnAssalto2.AutoSize = true;
+            this.btnAssalto2.Location = new System.Drawing.Point(401, 464);
+            this.btnAssalto2.Name = "btnAssalto2";
+            this.btnAssalto2.Size = new System.Drawing.Size(159, 24);
+            this.btnAssalto2.TabIndex = 10;
+            this.btnAssalto2.TabStop = true;
+            this.btnAssalto2.Text = "Fuzil de Assalto 2";
+            this.btnAssalto2.UseVisualStyleBackColor = true;
+            this.btnAssalto2.Visible = false;
+            // 
+            // btnEscopeta2
+            // 
+            this.btnEscopeta2.AutoSize = true;
+            this.btnEscopeta2.Location = new System.Drawing.Point(951, 464);
+            this.btnEscopeta2.Name = "btnEscopeta2";
+            this.btnEscopeta2.Size = new System.Drawing.Size(115, 24);
+            this.btnEscopeta2.TabIndex = 11;
+            this.btnEscopeta2.TabStop = true;
+            this.btnEscopeta2.Text = "Escopeta 2";
+            this.btnEscopeta2.UseVisualStyleBackColor = true;
+            this.btnEscopeta2.Visible = false;
+            // 
+            // btnAssalto3
+            // 
+            this.btnAssalto3.AutoSize = true;
+            this.btnAssalto3.Location = new System.Drawing.Point(401, 721);
+            this.btnAssalto3.Name = "btnAssalto3";
+            this.btnAssalto3.Size = new System.Drawing.Size(159, 24);
+            this.btnAssalto3.TabIndex = 12;
+            this.btnAssalto3.TabStop = true;
+            this.btnAssalto3.Text = "Fuzil de Assalto 3";
+            this.btnAssalto3.UseVisualStyleBackColor = true;
+            this.btnAssalto3.Visible = false;
+            // 
+            // btnEscopeta3
+            // 
+            this.btnEscopeta3.AutoSize = true;
+            this.btnEscopeta3.Location = new System.Drawing.Point(951, 720);
+            this.btnEscopeta3.Name = "btnEscopeta3";
+            this.btnEscopeta3.Size = new System.Drawing.Size(115, 24);
+            this.btnEscopeta3.TabIndex = 13;
+            this.btnEscopeta3.TabStop = true;
+            this.btnEscopeta3.Text = "Escopeta 3";
+            this.btnEscopeta3.UseVisualStyleBackColor = true;
+            this.btnEscopeta3.Visible = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(346, 881);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(98, 34);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(1123, 897);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(98, 34);
+            this.btnSair.TabIndex = 15;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 943);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnEscopeta3);
+            this.Controls.Add(this.btnAssalto3);
+            this.Controls.Add(this.btnEscopeta2);
+            this.Controls.Add(this.btnAssalto2);
+            this.Controls.Add(this.btnEscopeta1);
+            this.Controls.Add(this.btnEscopetas);
+            this.Controls.Add(this.btnAssalto1);
+            this.Controls.Add(this.btnFuzilsDeAssalto);
+            this.Controls.Add(this.pictureBoxEscopeta3);
+            this.Controls.Add(this.pictureBoxEscopeta2);
+            this.Controls.Add(this.pictureBoxEscopeta1);
+            this.Controls.Add(this.pictureBoxAssalto3);
+            this.Controls.Add(this.pictureBoxAssalto2);
+            this.Controls.Add(this.pictureBoxAssalto1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAssalto3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEscopeta3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxAssalto1;
+        private System.Windows.Forms.PictureBox pictureBoxAssalto2;
+        private System.Windows.Forms.PictureBox pictureBoxAssalto3;
+        private System.Windows.Forms.PictureBox pictureBoxEscopeta1;
+        private System.Windows.Forms.PictureBox pictureBoxEscopeta2;
+        private System.Windows.Forms.PictureBox pictureBoxEscopeta3;
+        private System.Windows.Forms.RadioButton btnFuzilsDeAssalto;
+        private System.Windows.Forms.RadioButton btnAssalto1;
+        private System.Windows.Forms.RadioButton btnEscopetas;
+        private System.Windows.Forms.RadioButton btnEscopeta1;
+        private System.Windows.Forms.RadioButton btnAssalto2;
+        private System.Windows.Forms.RadioButton btnEscopeta2;
+        private System.Windows.Forms.RadioButton btnAssalto3;
+        private System.Windows.Forms.RadioButton btnEscopeta3;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picturebox_e_radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxAssalto1.Visible = true;
+            btnAssalto1.Visible = true;
+            pictureBoxAssalto2.Visible = true;
+            btnAssalto2.Visible = true;
+            pictureBoxAssalto3.Visible = true;
+            btnAssalto3.Visible = true;
+        }
+
+        private void btnEscopetas_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBoxEscopeta1.Visible = true;
+            btnEscopeta1.Visible = true;
+            pictureBoxEscopeta2.Visible = true;
+            btnEscopeta2.Visible = true;
+            pictureBoxEscopeta3.Visible = true;
+            btnEscopeta3.Visible = true;
+        }
+    }
+}
